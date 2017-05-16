26717f831d79a27204bcb9e2158cda4e2994ddeb
Dmitry Meshcheryakov <dmitri.meshcheryakov@gmail.com>
2017-05-16 09:32:02 +0600

    Add tooltips on map

--- src/js/map.js
+++ src/js/map.js
@@ -1,6 +1,8 @@
 import * as d3 from 'd3';
 import * as topojson from "topojson-client";
-import data from '../world-topo-min.json';
+import world from '../world-topo-min.json';
+import data from '../data.json';
+import helpers from './helpers';
 
 export default (width, height) => {
   const map = d3.select('#map')
@@ -16,11 +18,46 @@ export default (width, height) => {
   const path = d3.geoPath()
     .projection(projection);
 
+  // tooltip
+  const tip = d3.select('body')
+    .append('div')
+      .attr('class', 'd3-tip')
+      .style('opacity', 0);
+
   const g = map.append('g');
   g.selectAll('path')
-    .data(topojson.feature(data, data.objects.countries).features)
+    .data(topojson.feature(world, world.objects.countries).features)
     .enter().append('path')
-      .attr('d', path);
+      .attr('d', path)
+      .attr('fill', d => (
+        helpers.didCountryHaveWorldCup(data, d.properties.name)
+          ? '#bf360c'
+          : '#254e77'
+      ))
+      .on('mouseover', d => {
+        if (helpers.didCountryHaveWorldCup(data, d.properties.name)) {
+          tip.transition()
+            .duration(200)
+            .style('opacity', 1);
+          tip
+            .html(
+              `<strong>
+                ${d.properties.name}:
+              </strong>
+              <ul>
+                ${helpers.getWorldCupYears(data, d.properties.name).map(year =>
+                  (`<li>${year}</li>`)
+                ).join('')}
+              </ul>`
+            )
+            .style('left', `${d3.event.pageX}px`)
+            .style('top', `${d3.event.pageY}px`);
+        }
+      })
+      .on('mouseout', () => {
+        tip
+          .style('opacity', 0);
+      });
 
   map.call(d3.zoom().on('zoom', () => g.attr('transform', d3.event.transform)));
 }
